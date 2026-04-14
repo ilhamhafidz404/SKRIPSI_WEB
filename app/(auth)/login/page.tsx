909016e3270e68707e46bf4b428b44ac8aee4242
ilhamhafidz404 <xxspanzie@egmail.com>
2026-04-14 21:53:12 +0700

    Adjustment auth page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import GoogleIcon from "@/components/icons/GoogleIcon";
 import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
@@ -22,26 +23,26 @@ export default function LoginPage() {
 
   if (status === "loading" || status === "authenticated") {
     return (
-      <div className="min-h-screen flex items-center justify-center bg-[#FFF]">
+      <div className="min-h-screen flex items-center justify-center bg-gray-50">
         <div className="w-9 h-9 border-2 border-[#C0202A] border-t-transparent rounded-full animate-spin" />
       </div>
     );
   }
 
   return (
-    <div className="relative min-h-screen flex flex-col items-center justify-center bg-[#FFF] px-4 overflow-hidden">
+    <div className="relative min-h-screen flex flex-col items-center justify-center bg-gray-50 px-4 overflow-hidden">
       {/* Background pattern */}
-      <div className="absolute inset-0 bg-[radial-gradient(circle,_rgba(192,32,42,0.08)_1px,_transparent_1px)] [background-size:28px_28px]" />
-      <div className="absolute inset-0 bg-[radial-gradient(ellipse_at_center,_transparent_40%,_#E8DDD5_100%)]" />
+      {/* <div className="absolute inset-0 bg-[radial-gradient(circle,_rgba(192,32,42,0.08)_1px,_transparent_1px)] [background-size:28px_28px]" />
+      <div className="absolute inset-0 bg-[radial-gradient(ellipse_at_center,_transparent_40%,_#E8DDD5_100%)]" /> */}
 
       {/* Card */}
-      <div className="relative w-full max-w-sm bg-[#FAF7F2] shadow-[0_0_0_1px_rgba(192,32,42,0.1),0_4px_6px_rgba(192,32,42,0.06),0_20px_60px_rgba(44,26,26,0.12)] overflow-hidden animate-[fadeUp_0.5s_ease]">
+      <div className="relative w-full max-w-sm bg-[#FFF] shadow-[0_0_0_1px_rgba(192,32,42,0.1),0_4px_6px_rgba(192,32,42,0.06),0_20px_60px_rgba(44,26,26,0.12)] overflow-hidden animate-[fadeUp_0.5s_ease]">
         {/* Top bar */}
         <div className="h-1 bg-gradient-to-r from-[#8B0000] via-[#C0202A] to-[#E8312A]" />
 
         <div className="px-10 py-9">
           {/* Logo */}
-          <div className="flex items-center gap-4 mb-5">
+          {/* <div className="flex items-center gap-4 mb-5">
             <div className="flex-1 h-px bg-gradient-to-r from-transparent to-[#C0202A40]" />
 
             <div className="w-14 h-14 rounded-full border border-[#C0202A25] bg-[#C0202A10] flex items-center justify-center">
@@ -49,7 +50,7 @@ export default function LoginPage() {
             </div>
 
             <div className="flex-1 h-px bg-gradient-to-l from-transparent to-[#C0202A40]" />
-          </div>
+          </div> */}
 
           {/* Brand */}
           <p className="text-[9px] tracking-[0.5em] text-[#C0202A] uppercase text-center font-mono mb-1">
@@ -65,14 +66,14 @@ export default function LoginPage() {
           </p>
 
           {/* Divider */}
-          <div className="flex items-center gap-3 my-6">
+          {/* <div className="flex items-center gap-3 my-6">
             <div className="flex-1 h-px bg-[#C0202A20]" />
             <div className="w-2 h-2 rotate-45 bg-[#C0202A50]" />
             <div className="flex-1 h-px bg-[#C0202A20]" />
-          </div>
+          </div> */}
 
           {/* Tagline */}
-          <p className="text-sm text-[#2C1A1A80] text-center leading-relaxed mb-6">
+          <p className="text-sm text-[#2C1A1A80] text-center leading-relaxed mb-6 mt-7">
             Sign in to access the dashboard and manage product authenticity
             records secured by blockchain.
           </p>
@@ -129,28 +130,5 @@ export default function LoginPage() {
         }
       `}</style>
     </div>
-  );
-}
-
-function GoogleIcon() {
-  return (
-    <svg width="18" height="18" viewBox="0 0 18 18">
-      <path
-        fill="#4285F4"
-        d="M17.64 9.2c0-.6-.05-1.2-.16-1.8H9v3.4h4.8c-.2 1.1-.8 2-1.7 2.7v2.2h2.9c1.7-1.5 2.6-3.8 2.6-6.5z"
-      />
-      <path
-        fill="#34A853"
-        d="M9 18c2.4 0 4.4-.8 5.9-2.1l-2.9-2.2c-.8.5-1.8.8-3 .8-2.3 0-4.3-1.6-5-3.7H1v2.3C2.6 16.5 5.6 18 9 18z"
-      />
-      <path
-        fill="#FBBC05"
-        d="M4 10.7c-.2-.5-.3-1.1-.3-1.7s.1-1.2.3-1.7V5H1C.4 6.2 0 7.6 0 9s.4 2.8 1 4l3-2.3z"
-      />
-      <path
-        fill="#EA4335"
-        d="M9 3.6c1.3 0 2.5.4 3.4 1.3l2.6-2.6C13.4.9 11.4 0 9 0 5.6 0 2.6 1.5 1 4l3 2.3c.7-2.1 2.7-3.7 5-3.7z"
-      />
-    </svg>
   );
 }
