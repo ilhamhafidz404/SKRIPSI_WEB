b078a2fc8e8a6187601a143db50e0fece99c6612
ilhamhafidz404 <xxspanzie@gmail.com>
2026-04-21 13:39:19 +0700

    Add website metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,16 @@ import { Outfit } from "next/font/google";
 import "./globals.css";
 import "flatpickr/dist/flatpickr.css";
 import AppProvider from "@/providers/AppProvider";
+import type { Metadata } from "next";
 
 const outfit = Outfit({
   subsets: ["latin"],
 });
+
+export const metadata: Metadata = {
+  title: "Certipath",
+  description: "Product Authenticity Verification Website",
+};
 
 export default function RootLayout({
   children,
